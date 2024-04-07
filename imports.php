0b17f5621aa10418292646ed36cc0cb83fb6bb26
marcio1002 <marcioalemao1999@yahoo.com>
2024-04-07 20:39:48 -0300

    fix: remove teste

--- imports.php
+++ imports.php
@@ -8,12 +8,10 @@ use Marcio1002\Work\Std;
 $GLOBALS['extensions_valid'] = ['csv', 'xlsx', 'xls', 'ods'];
 
 Std::write('Digite o diretório completo do arquivo excel do Clockfy: ');
-// $path_excel_import = Std::read();
-$path_excel_import = "/home/marcio/Documentos/Clockify_Relatório_De_Tempo_Detalhado_01_03_2024-31_03_2024.xlsx";
+$path_excel_import = Std::read();
 
 Std::write('Agora, digite o diretório completo que contém o modelo excel de "Relatório de Horas"');
-// $path_excel_export = Std::read();
-$path_excel_export = "/home/marcio/Documentos/modelo-gebit.xlsx";
+$path_excel_export = Std::read();
 
 Excel::import(
     read_file: $path_excel_import,
